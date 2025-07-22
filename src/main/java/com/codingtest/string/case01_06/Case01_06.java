4db55d0adde56ba111dafe7e5c5933d50489cc1d
baltol96 <baltol@naver.com>
2025-07-22 23:58:56 +0900

    중복문자제거

---
+++ src/main/java/com/codingtest/string/case01_06/Case01_06.java
@@ -0,0 +1,31 @@
+package com.codingtest.string.case01_06;
+
+import java.util.*;
+
+public class Case01_06 {
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String input = in.next();
+
+        char[] chars = input.toCharArray();
+
+        List<Character> charList = new ArrayList<>();
+
+        for(char c : chars) {
+
+            if(charList.contains(c)) continue;
+
+            charList.add(c);
+
+        }
+
+        char[] a = new char[charList.size()];
+        for(int i = 0; i < charList.size(); i++) {
+            a[i] = charList.get(i);
+        }
+
+        System.out.println(a);
+    }
+
+}
